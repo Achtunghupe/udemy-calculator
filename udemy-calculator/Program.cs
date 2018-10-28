372953d267fbf6275c6bd5e2ab9fed204e507be2
AchtungHuPe <achtunghupe@gmail.com>
2018-10-28 15:23:11 +0100

    Initialer Commit

---
+++ udemy-calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace udemy_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ziel: Zwei Ganzzahlen addieren
+            Console.Write("Erste Zahl eingeben: ");
+            string strFirstNumber = Console.ReadLine();
+            Console.Write("Zweite Zahl eingeben: ");
+            string strSecondNumber = Console.ReadLine();
+
+            // Eingabestring in Zahlen umwandeln
+            int intFirstNumber = Convert.ToInt32(strFirstNumber);
+            int intSecondNumber = Convert.ToInt32(strSecondNumber);
+
+            // Berechnung und Ausgabe
+            int sum = intFirstNumber + intSecondNumber;
+            Console.WriteLine("Das Ergebnis ist: {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
